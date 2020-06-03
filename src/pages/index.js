f9528208009f89e92f09a8c208b5736c1d0c7284
Chris Bird <chrisjbird@gmail.com>
2020-06-03 02:43:32 +0100

    change some stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Yo Dawg</h1>
+    <p>This is some Gatsby shizzle.</p>
+    <p>Now go build something wunderbar.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>Here's <Link to="/page-2/">page 2</Link>...</p>
   </Layout>
 )
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,8 +8,8 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <p>This is page 2</p>
+    <p>Go <Link to="/">back</Link> to the homepage</p>
   </Layout>
 )
 
